5a92099b397f7e62701248503e9cf7c19dd40918
jae woong <ijw9209@naver.com>
2022-12-16 16:53:28 +0900

    List ui 변경중

--- src/features/bankList/BankList.tsx
+++ src/features/bankList/BankList.tsx
@@ -2,6 +2,12 @@ import React, {useEffect} from 'react';
 import { bankList } from './bankListSlice';
 import { useAppDispatch } from '../../app/hooks';
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListSubheader from '@mui/material/ListSubheader';
+import ListItemText from '@mui/material/ListItemText';
+import Switch from '@mui/material/Switch';
+
 import { DataGrid, GridRowsProp , GridColDef ,GridToolbar } from '@mui/x-data-grid';
 
 export function BankList() {
@@ -25,7 +31,20 @@ export function BankList() {
 
     return (
         <div style={{ height: 300, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} />
+            {/* <DataGrid rows={rows} columns={columns} /> */}
+            <List
+                 sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+                 subheader={<ListSubheader>Settings</ListSubheader>}
+            >
+                 <ListItem>
+                    {/* 아이콘 영역 */}
+                 <ListItemText id="switch-list-label-wifi" primary="신한은행" />
+                {/*  */}
+                <Switch 
+                     edge="end"
+                />
+                 </ListItem>
+            </List>
         </div>
     );
 };
